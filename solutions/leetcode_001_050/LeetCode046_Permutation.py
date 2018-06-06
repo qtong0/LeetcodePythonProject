42faabc3906987481cab80b065888dc94a226c30
Qiang Tong <tongqiang.atp@gmail.com>
2018-06-05 23:11:44 -0400

    Adding solutions

--- solutions/leetcode_001_050/LeetCode046_Permutation.py
+++ solutions/leetcode_001_050/LeetCode046_Permutation.py
@@ -3,26 +3,24 @@ Created on Jan 19, 2017
 
 @author: MT
 '''
-
 class Solution(object):
     def permute(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        curr = []
-        result = []
-        if not nums: return result
-        self.dfs(nums, curr, result)
-        return result
+        res = []
+        if not nums: return res
+        self.dfs(nums, [], res)
+        return res
     
-    def dfs(self, nums, curr, result):
+    def dfs(self, nums, curr, res):
         if nums == []:
-            result.append(list(curr))
+            res.append(list(curr))
             return
         for i, num in enumerate(nums):
             curr.append(num)
-            self.dfs(nums[:i]+nums[i+1:], curr, result)
+            self.dfs(nums[:i]+nums[i+1:], curr, res)
             curr.pop()
     
     def test(self):
--- solutions/leetcode_001_050/LeetCode047_PermutationII.py
+++ solutions/leetcode_001_050/LeetCode047_PermutationII.py
@@ -3,31 +3,27 @@ Created on Jan 19, 2017
 
 @author: MT
 '''
-
 class Solution(object):
     def permuteUnique(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        if nums is None:
-            return []
-        if nums == []:
-            return [[]]
-        curr = []
-        result = []
-        self.dfs(sorted(nums), curr, result)
-        return result
+        if not nums: return []
+        if nums == []: return [[]]
+        res = []
+        self.dfs(sorted(nums), [], res)
+        return res
     
-    def dfs(self, nums, curr, result):
+    def dfs(self, nums, curr, res):
         if nums == []:
-            result.append(list(curr))
+            res.append(list(curr))
             return
         for i, num in enumerate(nums):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             curr.append(num)
-            self.dfs(nums[:i]+nums[i+1:], curr, result)
+            self.dfs(nums[:i]+nums[i+1:], curr, res)
             curr.pop()
     
     def test(self):
