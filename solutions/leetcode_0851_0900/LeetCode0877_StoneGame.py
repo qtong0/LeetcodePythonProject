fb387ba3edafbd4fcbdc637596aad8ed9c5b8a0e
Qiang Tong <tongqiang.atp@gmail.com>
2019-10-13 22:38:53 -0400

    adding solution

--- solutions/leetcode_0851_0900/LeetCode0877_StoneGame.py
+++ solutions/leetcode_0851_0900/LeetCode0877_StoneGame.py
@@ -9,29 +9,14 @@ class Solution(object):
         :type piles: List[int]
         :rtype: bool
         """
-        mem = {}
-        return self.dfs(mem, piles, 0, [0, 0], sum(piles) / 2.0)
-    
-    def dfs(self, mem, piles, idx, nums, target):
-        if not piles:
-            return nums[idx%2] > target
-        h = tuple(piles)
-        if h in mem: return mem[h]
-        if nums[idx%2] > target:
-            mem[h] = True
-            return True
-        if nums[(idx+1)%2] > target:
-            mem[h] = False
-            return False
-        for i in (0, -1):
-            nums[idx%2] += piles[i]
-            if not self.dfs(mem, piles, idx+1, nums, target) or \
-            not self.dfs(mem, piles, idx+1, nums, target):
-                mem[h] = True
-                return True
-            nums[idx%2] -= piles[i]
-        mem[h] = False
-        return False
+        n = len(piles)
+        dp = [[0]*n for _ in range(n)]
+        for i in range(n):
+            dp[i][i] = piles[i]
+        for d in range(1, n):
+            for i in range(n-d):
+                dp[i][i+d] = max(piles[i]-dp[i+1][i+d], piles[i+d]-dp[i][i+d-1])
+        return dp[0][-1] > 0
     
     def test(self):
         testCases = [
