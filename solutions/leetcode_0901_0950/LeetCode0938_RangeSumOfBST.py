51afb239eae3e93809ad9c863d18713d21faf49c
Qiang Tong <tongqiang.atp@gmail.com>
2020-01-11 22:04:30 -0500

    add new solutions

---
+++ solutions/leetcode_0901_0950/LeetCode0938_RangeSumOfBST.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def rangeSumBST(self, root, L, R):
+        """
+        :type root: TreeNode
+        :type L: int
+        :type R: int
+        :rtype: int
+        """
+        if not root:
+            return 0
+        val = root.val
+        if val < L:
+            return self.rangeSumBST(root.right, L, R)
+        elif val > R:
+            return self.rangeSumBST(root.left, L, R)
+        else:
+            return val + self.rangeSumBST(root.left, L, R) + \
+                self.rangeSumBST(root.right, L, R)
