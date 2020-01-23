42c97ba934ebef4bf30b0c0e1fc3e693a67a9ec5
Qiang Tong <tongqiang.atp@gmail.com>
2020-01-22 21:44:54 -0500

    add new solution

---
+++ solutions/leetcode_0951_1000/LeetCode0963_MinimumAreaRectangleII.py
@@ -0,0 +1,18 @@
+import itertools
+
+
+class Solution:
+    def minAreaFreeRect(self, points: list[list[int]]) -> float:
+        EPS = 1e-7
+        points = set(map(tuple, points))
+        res = float('inf')
+        for p1, p2, p3 in itertools.permutations(points, 3):
+            p4 = p2[0]+p3[0]-p1[0], p2[1]+p3[1]-p1[1]
+            if p4 in points:
+                v21 = complex(p2[0]-p1[0], p2[1]-p1[1])
+                v31 = complex(p3[0]-p1[0], p3[1]-p1[1])
+                if abs(v21.real * v31.real + v21.imag * v31.imag) < EPS:
+                    area = abs(v21) * abs(v31)
+                    if area < res:
+                        res = area
+        return res if res < float('inf') else 0
