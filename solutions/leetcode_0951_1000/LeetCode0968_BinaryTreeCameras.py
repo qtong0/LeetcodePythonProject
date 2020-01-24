6c2ce9f5d4ebcf4259f036df939f968912b6d0e8
Qiang Tong <tongqiang.atp@gmail.com>
2020-01-23 22:43:54 -0500

    add new solution

---
+++ solutions/leetcode_0951_1000/LeetCode0968_BinaryTreeCameras.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def minCameraCover(self, root: TreeNode) -> int:
+        def solve(node):
+            # 0: strict; all nodes below this are covered, but not this one
+            # 1: normal; all nodes below and including this are covered - no camera
+            # 2: placed camera: all nodes below this are covered, plus camera here
+            if not node: return 0, 0, float('inf')
+            l = solve(node.left)
+            r = solve(node.right)
+
+            dp0 = l[1] + r[1]
+            dp1 = min(l[2] + min(r[1:]), r[2] + min(l[1:]))
+            dp2 = 1 + min(l) + min(r)
+
+            return dp0, dp1, dp2
+
+        return min(solve(root)[1:])
