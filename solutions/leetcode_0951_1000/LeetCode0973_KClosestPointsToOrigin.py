1a80c26abe00c42cf407b39f6ccffe4a9227ebfb
Qiang Tong <tongqiang.atp@gmail.com>
2020-01-27 21:17:01 -0500

    add new solution

--- solutions/leetcode_0951_1000/LeetCode0973_KClosestPointsToOrigin.py
+++ solutions/leetcode_0951_1000/LeetCode0973_KClosestPointsToOrigin.py
@@ -3,6 +3,36 @@ import heapq
 
 class Solution:
     def kClosest(self, points, K):
+        dist = lambda i: points[i][0]**2 + points[i][1]**2
+
+        def sort(i, j, K):
+            if i >= j: return
+            mid = partition(i, j)
+            if K < mid-i+1:
+                sort(i, mid-1, K)
+            elif K > mid-i+1:
+                sort(mid+1, j, K-(mid-i+1))
+
+        def partition(i, j):
+            oi = i
+            pivot = dist(i)
+            i += 1
+
+            while True:
+                while i < j and dist(i) < pivot:
+                    i += 1
+                while i <= j and dist(j) >= pivot:
+                    j -= 1
+                if i >= j: break
+                points[i], points[j] = points[j], points[i]
+
+            points[oi], points[j] = points[j], points[oi]
+            return j
+
+        sort(0, len(points)-1, K)
+        return points[:K]
+
+    def kClosest_heap(self, points, K):
         heap = []
         for p in points:
             heapq.heappush(heap, (p[0]**2 + p[1]**2, p))
