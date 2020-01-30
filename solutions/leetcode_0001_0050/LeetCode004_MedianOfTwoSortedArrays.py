58b7760d484f112782e295841dd1dda8aaaeecdd
Qiang Tong <tongqiang.atp@gmail.com>
2020-01-29 22:46:20 -0500

    add new solution

--- solutions/leetcode_0001_0050/LeetCode004_MedianOfTwoSortedArrays.py
+++ solutions/leetcode_0001_0050/LeetCode004_MedianOfTwoSortedArrays.py
@@ -3,7 +3,61 @@ Created on Nov 8, 2017
 
 @author: MT
 '''
+
 class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        if len(nums1) > len(nums2):
+            return self.findMedianSortedArrays(nums2, nums1)
+        x, y = len(nums1), len(nums2)
+        low, high = 0, x
+        while low <= high:
+            partitionX = (low+high)//2
+            partitionY = (x+y+1)//2 - partitionX
+
+            # if partitioniX is 0 it means nothing is there on left side. Use -INF for maxLeftX
+            # if partitionX is length of input then there is nothing on right side. Use +INF for minRightX
+            maxLeftX = float('-inf') if partitionX == 0 else nums1[partitionX-1]
+            minRightX = float('inf') if partitionX == x else nums1[partitionX]
+
+            maxLeftY = float('-inf') if partitionY == 0 else nums2[partitionY-1]
+            minRightY = float('inf') if partitionY == y else nums2[partitionY]
+
+            if maxLeftX <= minRightY and maxLeftY <= minRightX:
+                # bingo!
+                if (x+y) % 2 == 0:
+                    return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2.0
+                else:
+                    return max(maxLeftX, maxLeftY)
+            elif maxLeftX > minRightY:
+                # Far too right on nums1, Need to go left!
+                high = partitionX - 1
+            else:
+                low = partitionX + 1
+
+    def test(self):
+        testCases = [
+            [
+                [1, 3],
+                [2],
+            ],
+            [
+                [1, 2],
+                [3, 4],
+            ],
+        ]
+        for nums1, nums2 in testCases:
+            print('nums1: %s' % nums1)
+            print('nums2: %s' % nums2)
+            result = self.findMedianSortedArrays(nums1, nums2)
+            print('result: %s' % result)
+            print('-='*30+'-')
+
+class Solution_lc_orig(object):
     def findMedianSortedArrays(self, nums1, nums2):
         """
         :type nums1: List[int]
@@ -37,24 +91,7 @@ class Solution(object):
                 else:
                     min_of_right = min(nums1[i], nums2[j])
                 return (max_of_left + min_of_right)/2.0
-    
-    def test(self):
-        testCases = [
-            [
-                [1, 3],
-                [2],
-            ],
-            [
-                [1, 2],
-                [3, 4],
-            ],
-        ]
-        for nums1, nums2 in testCases:
-            print('nums1: %s' % nums1)
-            print('nums2: %s' % nums2)
-            result = self.findMedianSortedArrays(nums1, nums2)
-            print('result: %s' % result)
-            print('-='*30+'-')
+
 
 if __name__ == '__main__':
     Solution().test()
