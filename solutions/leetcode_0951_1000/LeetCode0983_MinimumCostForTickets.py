30c6b7faf7e8825de5816027f28913f1f19c78e2
Qiang Tong <tongqiang.atp@gmail.com>
2020-01-31 00:04:09 -0500

    add new solution

--- solutions/leetcode_0951_1000/LeetCode0983_MinimumCostForTickets.py
+++ solutions/leetcode_0951_1000/LeetCode0983_MinimumCostForTickets.py
@@ -11,8 +11,8 @@ class Solution:
             if i not in daysSet:
                 dp[i] = dp[i-1]
             else:
-                dp[i] = min(dp[i-1] + costs[0], \
-                            dp[max(i-7, 0)] + costs[1], \
+                dp[i] = min(dp[i-1] + costs[0],
+                            dp[max(i-7, 0)] + costs[1],
                             dp[max(i-30, 0)] + costs[2])
         return dp[-1]
 
