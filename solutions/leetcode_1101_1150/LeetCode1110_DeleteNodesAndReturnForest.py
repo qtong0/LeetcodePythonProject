bfefb7dbc18062a3ee3d95ca1d25f9e840147894
Qiang Tong <tongqiang.atp@gmail.com>
2020-02-12 21:57:43 -0500

    add new solution

---
+++ solutions/leetcode_1101_1150/LeetCode1110_DeleteNodesAndReturnForest.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x, left=None, right=None):
+        self.val = x
+        self.left = left
+        self.right = right
+
+class Solution:
+    def delNodes(self, root: TreeNode, to_delete):
+        if not root: return []
+        hashset = set(to_delete)
+        res, queue = set([root]), []
+        queue = [(root, None)]
+        while queue:
+            node, parent = queue.pop(0)
+            if node.val in hashset:
+                res.discard(node)
+                if node.left:
+                    res.add(node.left)
+                if node.right:
+                    res.add(node.right)
+                if parent:
+                    if parent.left == node:
+                        parent.left = None
+                    else:
+                        parent.right = None
+            if node.left:
+                queue.append((node.left, node))
+            if node.right:
+                queue.append((node.right, node))
+        return list(res)
