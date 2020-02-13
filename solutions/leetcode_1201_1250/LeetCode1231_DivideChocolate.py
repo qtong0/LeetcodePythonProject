5d957bbf8f079f5467041e04af827ae81675ed17
Qiang Tong <tongqiang.atp@gmail.com>
2020-02-12 22:50:15 -0500

    add new solution

--- solutions/leetcode_1201_1250/LeetCode1231_DivideChocolate.py
+++ solutions/leetcode_1201_1250/LeetCode1231_DivideChocolate.py
@@ -9,10 +9,10 @@ class Solution:
                 r = mid - 1
         return l-1
 
-    def canSplit(self, nums, k, s):
+    def canSplit(self, nums, k, maxVal):
         splits, total = 0, 0
         for num in nums:
-            if total + num < s:
+            if total + num < maxVal:
                 total += num
             else:
                 total = 0
