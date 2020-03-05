3531f3a2cc04dc50f7ce82ac456ff7578be92070
Qiang Tong <tongqiang.atp@gmail.com>
2020-03-04 21:26:14 -0500

    add new solution

--- solutions/leetcode_0701_0750/LeetCode732_MyCalendarIII.py
+++ solutions/leetcode_0701_0750/LeetCode732_MyCalendarIII.py
@@ -1,11 +1,27 @@
-'''
-Created on Mar 6, 2018
-
-@author: tongq
-'''
 class MyCalendarThree(object):
     # Checkout Java Solution!
     pass
 
-if __name__ == '__main__':
-    pass
+
+"""
+
+class MyCalendarThree {
+    private TreeMap<Integer, Integer> timeline = new TreeMap<>();
+
+    public MyCalendarThree() {
+        return;
+    }
+    
+    public int book(int start, int end) {
+        timeline.put(start, timeline.getOrDefault(start, 0)+1);
+        timeline.put(end, timeline.getOrDefault(end, 0)-1);
+        int ongoing = 0, k = 0;
+        for (int v: timeline.values()) {
+        	ongoing += v;
+        	k = Math.max(k, ongoing);
+        }
+        return k;
+    }
+}
+
+"""
