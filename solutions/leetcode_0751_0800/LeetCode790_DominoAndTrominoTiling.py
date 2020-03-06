9ccd751b25e9ddbac34f22f306b8bd9dad90054a
Qiang Tong <tongqiang.atp@gmail.com>
2020-03-05 23:17:45 -0500

    add new solution

--- solutions/leetcode_0751_0800/LeetCode790_DominoAndTrominoTiling.py
+++ solutions/leetcode_0751_0800/LeetCode790_DominoAndTrominoTiling.py
@@ -1,10 +1,18 @@
-'''
-Created on Apr 15, 2018
-
-@author: tongq
-'''
 class Solution(object):
-    def numTilings(self, N):
+    def numTilings(self, N: int) -> int:
+        MOD = 10**9 + 7
+        dp = [1, 0, 0, 0]
+        for _ in range(N):
+            ndp = [0, 0, 0, 0]
+            ndp[0b00] = (dp[0b00] + dp[0b11]) % MOD
+            ndp[0b01] = (dp[0b00] + dp[0b10]) % MOD
+            ndp[0b10] = (dp[0b00] + dp[0b01]) % MOD
+            ndp[0b11] = (dp[0b00] + dp[0b01] + dp[0b10]) % MOD
+            dp = ndp
+        return dp[0]
+
+
+    def numTilings_another(self, N):
         """
         :type N: int
         :rtype: int
