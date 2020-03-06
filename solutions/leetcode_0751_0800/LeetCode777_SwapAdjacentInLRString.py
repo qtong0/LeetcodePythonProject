ee7bb9172f9677b33d51497f69910cf8d4f9663b
Qiang Tong <tongqiang.atp@gmail.com>
2020-03-05 22:12:32 -0500

    add new solution

--- solutions/leetcode_0751_0800/LeetCode777_SwapAdjacentInLRString.py
+++ solutions/leetcode_0751_0800/LeetCode777_SwapAdjacentInLRString.py
@@ -10,37 +10,6 @@ class Solution(object):
         :type end: str
         :rtype: bool
         """
-        import re
-        if re.sub('X', '', start) != re.sub('X', '', end):
-            return False
-        p1, p2 = 0, 0
-        while p1 < len(start) and p2 < len(end):
-            while p1 < len(start) and start[p1] == 'X':
-                p1 += 1
-            while p2 < len(end) and end[p2] == 'X':
-                p2 += 1
-            if p1 == len(start) and p2 == len(end):
-                return True
-            if p1 == len(start) or p2 == len(end):
-                return False
-            if start[p1] != end[p2]:
-                return False
-            # if the character is 'L', it can only be moved to the left. p1 should be greater or equal to p2.
-            if start[p1] == 'L' and p2 > p1:
-                return False
-            # if the character is 'R', it can only be moved to the right. p2 should be greater or equal to p1.
-            if start[p1] == 'R' and p1 > p2:
-                return False
-            p1 += 1
-            p2 += 1
-        return True
-    
-    def canTransform_another(self, start, end):
-        """
-        :type start: str
-        :type end: str
-        :rtype: bool
-        """
         l, r = 0, 0
         for i in range(len(start)):
             if start[i] == 'R': r += 1
@@ -52,7 +21,8 @@ class Solution(object):
         if l == 0 and r == 0:
             return True
         return False
-    
+
+
     # The following BFS solution is TLE
     
     def canTransform_bfs_TLE(self, start, end):
