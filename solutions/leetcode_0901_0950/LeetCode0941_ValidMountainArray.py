e0efed44671fc8db14be46dbf71b36a4355c68b7
Qiang Tong <tongqiang.atp@gmail.com>
2020-03-08 23:24:40 -0400

    add new solution

--- solutions/leetcode_0901_0950/LeetCode0941_ValidMountainArray.py
+++ solutions/leetcode_0901_0950/LeetCode0941_ValidMountainArray.py
@@ -4,10 +4,15 @@ class Solution(object):
         :type A: List[int]
         :rtype: bool
         """
-        i = 0
+        if not A or len(A) < 3:
+            return False
+        if A[0] > A[1]:
+            return False
+        i = 1
         while i < len(A) and A[i-1] < A[i]:
             i += 1
-        if i == 0:
+        if i == 1 or i == len(A):
             return False
-        while i < len(A) and A[i-1] > A[i]:
+        while i < len(A) and (A[i-1] > A[i]):
             i += 1
+        return i == len(A)
