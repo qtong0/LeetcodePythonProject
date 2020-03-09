729bd419425676b1fbc27707333565771d4e1282
Qiang Tong <tongqiang.atp@gmail.com>
2020-03-09 00:17:55 -0400

    add new solution

--- solutions/leetcode_0951_1000/LeetCode0996_NumberOfSquarefulArrays.py
+++ solutions/leetcode_0951_1000/LeetCode0996_NumberOfSquarefulArrays.py
@@ -1,10 +1,24 @@
-import math
+import math, collections
 
 
 class Solution:
+    def numSquarefulPerms(self, A) -> int:
+        c = collections.Counter(A)
+        cand = {i: {j for j in c if int((i + j)**0.5) ** 2 == i + j} for i in c}
+        self.res = 0
+        def dfs(x, left=len(A) - 1):
+            c[x] -= 1
+            if left == 0: self.res += 1
+            for y in cand[x]:
+                if c[y]: dfs(y, left - 1)
+            c[x] += 1
+        for x in c: dfs(x)
+        return self.res
+
+
     # this solution is TLE only
     # needs to check later to get best solution
-    def numSquarefulPerms(self, A) -> int:
+    def numSquarefulPerms_own_TLE(self, A) -> int:
         res = set()
         self.helper(res, [], A, len(A))
         return len(res)
