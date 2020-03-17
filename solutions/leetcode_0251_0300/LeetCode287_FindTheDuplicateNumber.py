62f2d03f4db3448f7c911a8c4faeef30adb5a3fb
Qiang Tong <tongqiang.atp@gmail.com>
2020-03-16 22:23:21 -0400

    add new solution

--- solutions/leetcode_0251_0300/LeetCode287_FindTheDuplicateNumber.py
+++ solutions/leetcode_0251_0300/LeetCode287_FindTheDuplicateNumber.py
@@ -1,24 +1,17 @@
-'''
-Created on Mar 7, 2017
-
-@author: MT
-'''
-
 class Solution(object):
     def findDuplicate(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums) > 1:
-            slow, fast = nums[0], nums[nums[0]]
-            while slow != fast:
-                slow = nums[slow]
-                fast = nums[nums[fast]]
-            fast = 0
-            while fast != slow:
-                fast = nums[fast]
-                slow = nums[slow]
-            return slow
-        return -1
-    
+        val = nums[0]
+        nextVal = nums[val]
+        while val != nextVal:
+            val = nums[val]
+            nextVal = nums[nextVal]
+            nextVal = nums[nextVal]
+        val0 = 0
+        while val0 != val:
+            val0 = nums[val0]
+            val = nums[val]
+        return val
