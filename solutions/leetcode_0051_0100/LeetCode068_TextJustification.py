b8d73a4c22e610e9579d55c473a559a48a48364c
Qiang Tong <tongqiang.atp@gmail.com>
2020-03-24 17:52:05 -0400

    add new solution

--- solutions/leetcode_0051_0100/LeetCode068_TextJustification.py
+++ solutions/leetcode_0051_0100/LeetCode068_TextJustification.py
@@ -1,10 +1,24 @@
-'''
-Created on Jan 22, 2017
-
-@author: MT
-'''
 class Solution(object):
     def fullJustify(self, words, maxWidth):
+        res, cur, num_of_letters = [], [], 0
+        for w in words:
+            if num_of_letters + len(w) + len(cur) > maxWidth:
+                if len(cur) == 1:
+                    res.append( cur[0] + ' '*(maxWidth - num_of_letters) )
+                else:
+                    num_spaces = maxWidth - num_of_letters
+                    space_between_words, num_extra_spaces = divmod( num_spaces, len(cur)-1 )
+                    for i in range(num_extra_spaces):
+                        cur[i] += ' '
+                    res.append((' '*space_between_words).join(cur))
+                cur, num_of_letters = [], 0
+            cur += [w]
+            num_of_letters += len(w)
+        res.append(' '.join(cur) + ' '*(maxWidth - num_of_letters - len(cur) + 1))
+        return res
+
+
+    def fullJustify_another(self, words, maxWidth):
         """
         :type words: List[str]
         :type maxWidth: int
@@ -20,51 +34,8 @@ class Solution(object):
             cur += [w]
             num_of_letters += len(w)
         return res + [' '.join(cur).ljust(maxWidth)]
-    
-    def fullJustify_orig(self, words, maxWidth):
-        """
-        :type words: List[str]
-        :type maxWidth: int
-        :rtype: List[str]
-        """
-        result = []
-        if not words: return result
-        count, last = 0, 0
-        for i, word in enumerate(words):
-            count += len(word)
-            if count+i-last > maxWidth:
-                wordsLen = count-len(word)
-                spaceLen = maxWidth-wordsLen
-                eachLen = 1
-                extraLen = 0
-                if i-last-1>0:
-                    eachLen = spaceLen/(i-last-1)
-                    extraLen = spaceLen%(i-last-1)
-                s = ''
-                for k in range(last, i-1):
-                    s += words[k]
-                    ce = 0
-                    while ce < eachLen:
-                        s += ' '
-                        ce += 1
-                    if extraLen > 0:
-                        s += ' '
-                        extraLen -= 1
-                s += words[i-1]
-                while len(s) < maxWidth:
-                    s += ' '
-                result.append(s)
-                last = i
-                count = len(word)
-        s = ''
-        for i in range(last, len(words)-1):
-            count += len(words[i])
-            s += words[i] + ' '
-        s += words[-1]
-        s += ' '*(maxWidth-len(s))
-        result.append(s)
-        return result
-    
+
+
     def test(self):
         testCases = [
             (["This", "is", "an", "example", "of", "text", "justification."], 16),
