69cc1f0ce0826ee8ab77750d4978ccfe0608a899
Qiang Tong <tongqiang.atp@gmail.com>
2020-03-28 21:38:05 -0400

    add new solution

---
+++ solutions/leetcode_1001_1050/LeetCode1004_MaxConsecutiveOnesIII.py
@@ -0,0 +1,33 @@
+class Solution:
+    def longestOnes(self, A, K: int) -> int:
+        queue = []
+        res = 0
+        left = -1
+        for i, num in enumerate(A):
+            if num == 0:
+                queue.append(i)
+                res = max(res, i-left-1)  # Check this first
+                if len(queue) > K:
+                    left = queue.pop(0)
+        res = max(res, len(A)-left-1)
+        return res
+
+    def test(self):
+        testCases = [
+            [[0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], 3],
+            [[1,1,1,0,0,0,1,1,1,1,0], 2],
+            [[0,0,1,1,1,0,0], 0],
+            [[1,1,1,1], 0],
+            [[0,0,1,1,1,0,0,1,1,1,1], 0],
+            [[0,0,0,1], 4],
+            [[0,0,0,1], 2],
+            [[1,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,1,1,1,1,0,1,0,1,1,1,1,1,1,0,1,0,1,0,0,1,1,0,1,1], 8],
+        ]
+        for arr, k in testCases:
+            res = self.longestOnes(arr, k)
+            print('res: %s' % res)
+            print('-='*30+'-')
+
+
+if __name__ == '__main__':
+    Solution().test()
