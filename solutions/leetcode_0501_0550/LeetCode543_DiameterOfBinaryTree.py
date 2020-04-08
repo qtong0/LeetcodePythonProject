64a54da1e47978692c14f24a726c562b974644d3
Qiang Tong <tongqiang.atp@gmail.com>
2020-04-07 20:32:19 -0400

    add new solution

--- solutions/leetcode_0501_0550/LeetCode543_DiameterOfBinaryTree.py
+++ solutions/leetcode_0501_0550/LeetCode543_DiameterOfBinaryTree.py
@@ -1,15 +1,10 @@
-'''
-Created on Aug 20, 2017
-
-@author: MT
-'''
-
 # Definition for a binary tree node.
 class TreeNode(object):
     def __init__(self, x, left=None, right=None):
         self.val = x
         self.left = left
         self.right = right
+
 
 class Solution(object):
     def diameterOfBinaryTree(self, root):
@@ -18,17 +13,17 @@ class Solution(object):
         :rtype: int
         """
         if not root: return 0
-        return self.helper(root)[-1]-1
-    
-    # returns include, exclude
+        self.diameter = 0
+        self.helper(root)
+        return self.diameter-1
+
     def helper(self, root):
         if not root:
-            return 0, 0
-        left = self.helper(root.left)
-        right = self.helper(root.right)
-        include = max(1+left[0], 1+right[0])
-        exclude = max([left[1], right[1], left[0]+right[0]+1])
-        return include, exclude
+            return 0
+        l = self.helper(root.left)
+        r = self.helper(root.right)
+        self.diameter = max(self.diameter, l+r+1)
+        return 1+max(l, r)
     
     def test(self):
         testCases = [
@@ -38,6 +33,7 @@ class Solution(object):
             result = self.diameterOfBinaryTree(root)
             print('result: %s' % result)
             print('-='*30+'-')
+
 
 if __name__ == '__main__':
     Solution().test()
