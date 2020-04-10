8d77495f55a945e3a3f0af6d216b4ac32f63b011
Qiang Tong <tongqiang.atp@gmail.com>
2020-04-10 00:14:22 -0400

    add new solution

--- solutions/leetcode_1251_1300/LeetCode1269_NumberOfWaysToStayInTheSamePlaceAfterSomeSteps.py
+++ solutions/leetcode_1251_1300/LeetCode1269_NumberOfWaysToStayInTheSamePlaceAfterSomeSteps.py
@@ -1,5 +1,23 @@
 class Solution:
+    # improved way
     def numWays(self, steps: int, arrLen: int) -> int:
+        MOD = 10**9+7
+        last = [0]*(min(steps, arrLen)+1)
+        last[0] = 1
+        for i in range(1, steps+1):
+            curr = [0]*(min(steps, arrLen)+1)
+            for j in range(min(steps, arrLen)):
+                if last[j] > 0:
+                    curr[j] = (curr[j] + last[j]) % MOD
+                if j+1 < arrLen and last[j+1] > 0:
+                    curr[j] = (curr[j] + last[j+1]) % MOD
+                if j-1 >= 0 and last[j-1] > 0:
+                    curr[j] = (curr[j] + last[j-1]) % MOD
+            last = curr
+        return last[0]
+
+    # DP solution first with extra space
+    def numWays_own(self, steps: int, arrLen: int) -> int:
         MOD = 10**9+7
         dp = [[0]*arrLen for _ in range(steps+1)]
         dp[0][0] = 1
