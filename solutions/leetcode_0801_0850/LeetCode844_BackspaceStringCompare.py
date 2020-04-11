e8516a49a8b3022548a128f467c383b0bd8850c4
Qiang Tong <tongqiang.atp@gmail.com>
2020-04-11 14:02:54 -0400

    add new solution

--- solutions/leetcode_0801_0850/LeetCode844_BackspaceStringCompare.py
+++ solutions/leetcode_0801_0850/LeetCode844_BackspaceStringCompare.py
@@ -1,37 +1,58 @@
-'''
-Created on Mar 18, 2019
-
-@author: tongq
-'''
 class Solution(object):
-    def backspaceCompare(self, S, T):
-        """
-        :type S: str
-        :type T: str
-        :rtype: bool
-        """
-        s, t = S, T
-        s0, t0 = '', ''
-        for c in s:
-            if c == '#':
-                s0 = s0[:-1]
-            else:
-                s0 += c
-        for c in t:
-            if c == '#':
-                t0 = t0[:-1]
-            else:
-                t0 += c
-        return s0 == t0
+    def backspaceCompare(self, S: str, T: str) -> bool:
+        i, j = len(S)-1, len(T)-1
+        skipS, skipT = 0, 0
+        while i >= 0 or j >= 0:
+            while i >= 0:
+                if S[i] == '#':
+                    skipS += 1
+                    i -= 1
+                elif skipS > 0:
+                    skipS -= 1
+                    i -= 1
+                else:
+                    break
+            while j >= 0:
+                if T[j] == '#':
+                    skipT += 1
+                    j -= 1
+                elif skipT > 0:
+                    skipT -= 1
+                    j -= 1
+                else:
+                    break
+            if i >= 0 and j >= 0 and S[i] != T[j]:
+                return False
+            if (i >= 0) != (j >= 0):
+                return False
+            i -= 1
+            j -= 1
+        return True
+
+
+    def backspaceCompare_SPACE(self, S: str, T: str) -> bool:
+        def build(s):
+            res = []
+            for c in s:
+                if c != '#':
+                    res.append(c)
+                else:
+                    res.pop()
+            return res
+        return build(S) == build(T)
     
     def test(self):
         testCases = [
-            
+            ["ab#c", "ad#c"],
+            ["ab##", "c#d#"],
+            ["a##c", "#a#c"],
+            ["a#c", "b"],
         ]
         for s, t in testCases:
             result = self.backspaceCompare(s, t)
             print('result: %s' % result)
             print('-='*30+'-')
+
 
 if __name__ == '__main__':
     Solution().test()
