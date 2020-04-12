4a0fcbeb691e93fb93a4fdd8242cd2642a5c5d44
Qiang Tong <tongqiang.atp@gmail.com>
2020-04-11 21:04:56 -0400

    add new solution

--- solutions/leetcode_0951_1000/LeetCode0973_KClosestPointsToOrigin.py
+++ solutions/leetcode_0951_1000/LeetCode0973_KClosestPointsToOrigin.py
@@ -3,34 +3,35 @@ import heapq
 
 class Solution:
     def kClosest(self, points, K):
-        dist = lambda i: points[i][0]**2 + points[i][1]**2
-
-        def sort(i, j, K):
-            if i >= j: return
-            mid = partition(i, j)
-            if K < mid-i+1:
-                sort(i, mid-1, K)
-            elif K > mid-i+1:
-                sort(mid+1, j, K-(mid-i+1))
-
-        def partition(i, j):
-            oi = i
-            pivot = dist(i)
-            i += 1
-
-            while True:
-                while i < j and dist(i) < pivot:
-                    i += 1
-                while i <= j and dist(j) >= pivot:
-                    j -= 1
-                if i >= j: break
-                points[i], points[j] = points[j], points[i]
-
-            points[oi], points[j] = points[j], points[oi]
-            return j
-
-        sort(0, len(points)-1, K)
+        if K < 1 or not points:
+            return []
+        self.helper(points, 0, len(points)-1, K-1)
         return points[:K]
+
+    def helper(self, points, i, j, k):
+        i0, j0 = i, j
+        pivot = self.dist(points[j])
+        while True:
+            while i < j and self.dist(points[i]) < pivot:
+                i += 1
+            while i < j and self.dist(points[j]) >= pivot:
+                j -= 1
+            if i < j:
+                points[i], points[j] = points[j], points[i]
+            else:
+                break
+        points[i], points[j0] = points[j0], points[i]
+        if i == k:
+            return
+        elif i < k:
+            self.helper(points, i+1, j0, k)
+        else:
+            self.helper(points, i0, i-1, k)
+
+    def dist(self, p):
+        return p[0]**2 + p[1]**2
+
+
 
     def kClosest_heap(self, points, K):
         heap = []
@@ -40,3 +41,22 @@ class Solution:
         for _ in range(K):
             res.append(heapq.heappop(heap)[1])
         return res
+
+
+    def test(self):
+        testCases = [
+            [[[1,3],[-2,2]], 1],
+            [[[3,3],[5,-1],[-2,4]], 2],
+            [
+                [[0,1],[1,0]],
+                2,
+            ],
+        ]
+        for points, k in testCases:
+            res = self.kClosest(points, k)
+            print('res: %s' % res)
+            print('-='*30+'-')
+
+
+if __name__ == '__main__':
+    Solution().test()
