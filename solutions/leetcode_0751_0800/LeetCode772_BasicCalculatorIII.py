1d26bcf7a1085286692118255b3a4ac4528bc0d7
Qiang Tong <tongqiang.atp@gmail.com>
2020-04-13 21:03:54 -0400

    add new solution

--- solutions/leetcode_0751_0800/LeetCode772_BasicCalculatorIII.py
+++ solutions/leetcode_0751_0800/LeetCode772_BasicCalculatorIII.py
@@ -1,55 +1,54 @@
-'''
-Created on Apr 5, 2018
-
-@author: tongq
-'''
 class Solution(object):
-    def calculate(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        l1, o1 = 0, 1
-        l2, o2 = 1, 1
-        deque = []
-        i = 0
-        while i < len(s):
-            c = s[i]
+    def calculate(self, s: str) -> int:
+        if not s: return 0
+        queue = []
+        for c in s:
+            queue.append(c)
+        queue.append('+')
+        return self.cal(queue)
+
+    def cal(self, queue):
+        sign = '+'
+        num = 0
+        stack = []
+        while queue:
+            c = queue.pop(0)
+            if c == ' ':
+                continue
             if c.isdigit():
-                num = ord(c)-ord('0')
-                while i+1 < len(s) and s[i+1].isdigit():
-                    i += 1
-                    num = num*10+ord(s[i])-ord('0')
-                l2 = l2*num if o2==1 else l2//num
+                num = 10*num + ord(c) - ord('0')
             elif c == '(':
-                deque.insert(0, l1)
-                deque.insert(0, o1)
-                deque.insert(0, l2)
-                deque.insert(0, o2)
-                l1, o1 = 0, 1
-                l2, o2 = 1, 1
-            elif c == ')':
-                num = l1+o1*l2
-                o2 = deque.pop(0)
-                l2 = deque.pop(0)
-                o1 = deque.pop(0)
-                l1 = deque.pop(0)
-                l2 = l2*num if o2 == 1 else l2//num
-            elif c == '*' or c == '/':
-                o2 = 1 if c=='*' else -1
-            elif c == '+' or c == '-':
-                l1 = l1 + o1*l2
-                o1 = 1 if c=='+' else -1
-                l2, o2 = 1, 1
-            i += 1
-        return l1+o1*l2
+                num = self.cal(queue)
+            else:
+                if sign == '+':
+                    stack.append(num)
+                elif sign == '-':
+                    stack.append(-num)
+                elif sign == '*':
+                    stack.append(stack.pop() * num)
+                elif sign == '/':
+                    tmp = stack.pop()
+                    if tmp % num != 0 and tmp*num < 0:
+                        stack.append(tmp // num+1)
+                    else:
+                        stack.append(tmp // num)
+                num = 0
+                sign = c
+                if c == ')':
+                    break
+        sumVal = 0
+        while stack:
+            sumVal += stack.pop()
+        return sumVal
     
     def test(self):
         testCases = [
-            "1 + 1",
+            "5 - 3/2",
             " 6-4 / 2 ",
+            "1 + 1",
             "2*(5+5*2)/3+(6/2+8)",
-            "(2+6* 3+5- (3*14/7+2)*5)+3"
+            "(2+6* 3+5- (3*14/7+2)*5)+3",
+            "-1+4*3/3/3",
         ]
         for s in testCases:
             print('s: %s' % s)
