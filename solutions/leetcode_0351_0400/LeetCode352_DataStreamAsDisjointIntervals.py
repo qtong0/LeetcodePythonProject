25871decfa718f10466afb81c19acaebf8775d2f
Qiang Tong <tongqiang.atp@gmail.com>
2020-04-13 21:48:54 -0400

    add new solution

--- solutions/leetcode_0351_0400/LeetCode352_DataStreamAsDisjointIntervals.py
+++ solutions/leetcode_0351_0400/LeetCode352_DataStreamAsDisjointIntervals.py
@@ -1,30 +1,47 @@
-'''
-Created on Mar 22, 2017
+import heapq
 
-@author: MT
-'''
-class Interval(object):
-    def __init__(self, s=0, e=0):
-        self.start = s
-        self.end = e
+class SummaryRanges:
 
-class SummaryRanges(object):
+    def __init__(self):
+        self.intervals = []
+
+    def addNum(self, val):
+        heapq.heappush(self.intervals, (val, [val, val]))
+
+    def getIntervals(self):
+        stack = []
+        while self.intervals:
+            idx, cur = heapq.heappop(self.intervals)
+            if not stack:
+                stack.append((idx, cur))
+            else:
+                _, prev = stack[-1]
+                if prev[1] + 1 >= cur[0]:
+                    prev[1] = max(prev[1], cur[1])
+                else:
+                    stack.append((idx, cur))
+        self.intervals = stack
+        return list(map(lambda x: x[1], stack))
+
+
+
+class SummaryRanges_own(object):
     def __init__(self):
         self.intervals = []
     
     def addNum(self, val):
         if not self.intervals:
-            self.intervals.append(Interval(val, val))
+            self.intervals.append([val, val])
         else:
             result = []
-            newInterval = Interval(val, val)
+            newInterval = [val, val]
             for interval in self.intervals:
-                if newInterval.end < interval.start-1:
+                if newInterval[1] < interval[0]-1:
                     result.append(newInterval)
                     newInterval = interval
-                elif newInterval.start <= interval.end+1:
-                    newInterval = Interval(min(interval.start, newInterval.start),\
-                        max(interval.end, newInterval.end))
+                elif newInterval[0] <= interval[1]+1:
+                    newInterval = [min(interval[0], newInterval[0]), \
+                                   max(interval[1], newInterval[1])]
                 else:
                     result.append(interval)
             result.append(newInterval)
