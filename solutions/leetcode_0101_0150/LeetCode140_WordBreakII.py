2b1f184e37274e5d42878bb632119d8c23414ba9
Qiang Tong <tongqiang.atp@gmail.com>
2020-04-17 23:14:49 -0400

    add new solution

--- solutions/leetcode_0101_0150/LeetCode140_WordBreakII.py
+++ solutions/leetcode_0101_0150/LeetCode140_WordBreakII.py
@@ -1,5 +1,25 @@
 class Solution(object):
     def wordBreak(self, s: str, wordDict):
+        wordDict = set(wordDict)
+        if not s: return []
+        n = len(s)
+        dp = [[] for _ in range(n+1)]
+        dp[0].append('')
+        for i in range(1, n+1):
+            curList = []
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
+                    for l in dp[j]:
+                        if l:
+                            curList.append(l + ' ' + s[j:i])
+                        else:
+                            curList.append(s[j:i])
+                dp[i] = curList
+        return dp[-1]
+
+
+    # My own solution, difficult to figure out the time complexity
+    def wordBreak_own(self, s: str, wordDict):
         if not s: return []
         n = len(s)
         dp = [[] for _ in range(n+1)]
@@ -8,7 +28,6 @@ class Solution(object):
             for w in wordDict:
                 if dp[i] and i+len(w) < n+1 and s[i:i+len(w)] == w:
                     dp[i+len(w)].append(w)
-        print('dp: %s' % dp)
         res = []
         self.helper(n, res, [], dp)
         return res
