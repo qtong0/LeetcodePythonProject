6edc05e52a0d4b06bf41b942c8c7f58ebdfd9f3f
Qiang Tong <tongqiang.atp@gmail.com>
2020-04-17 22:43:16 -0400

    add new solution

--- solutions/leetcode_1001_1050/LeetCode1004_MaxConsecutiveOnesIII.py
+++ solutions/leetcode_1001_1050/LeetCode1004_MaxConsecutiveOnesIII.py
@@ -1,5 +1,17 @@
 class Solution:
     def longestOnes(self, A, K: int) -> int:
+        i = 0
+        for j in range(len(A)):
+            if A[j] == 0:
+                K -= 1
+            if K < 0:
+                if A[i] == 0:
+                    K += 1
+                i += 1
+        return j-i+1
+
+
+    def longestOnes_Space(self, A, K: int) -> int:
         queue = []
         res = 0
         left = -1
