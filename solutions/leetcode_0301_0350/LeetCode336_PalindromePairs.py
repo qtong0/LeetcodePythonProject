42c1f0cfd0c77f0f2eb8d7b1db980090e6c2f10d
Qiang Tong <tongqiang.atp@gmail.com>
2020-04-19 14:55:19 -0400

    add new solution

--- solutions/leetcode_0301_0350/LeetCode336_PalindromePairs.py
+++ solutions/leetcode_0301_0350/LeetCode336_PalindromePairs.py
@@ -1,10 +1,68 @@
-'''
-Created on Mar 19, 2017
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.arr = []
+        self.idx = -1
+        self.children = {}
 
-@author: MT
-'''
 
-class Solution(object):
+class Solution:
+    # O (k^2 * N)
+    def palindromePairs(self, words):
+        self.root = Node(None)
+        for i, word in enumerate(words):
+            self.build(word, i)
+        res = []
+        for i, word, in enumerate(words):
+            self.search(word, res, i)
+        return res
+
+    def build(self, word, i):
+        node = self.root
+        for j in range(len(word)-1, -1, -1):
+            c = word[j]
+            if c not in node.children:
+                node.children[c] = Node(c)
+            if self.isPalindrome(word, 0, j):
+                node.arr.append(i)
+            node = node.children[c]
+        node.arr.append(i)
+        node.idx = i
+
+    def search(self, word, res, i):
+        node = self.root
+        for j, c in enumerate(word):
+            if node.idx >= 0 and node.idx != i and self.isPalindrome(word, j, len(word)-1):
+                res.append([i, node.idx])
+            if c not in node.children:
+                return
+            node = node.children[c]
+        for j in node.arr:
+            if i == j:
+                continue
+            res.append([i, j])
+
+    def isPalindrome(self, word, l, r):
+        while l < r:
+            if word[l] != word[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
+
+    def test(self):
+        testCases = [
+            ["abcd", "dcba", "lls", "s", "sssll"],
+        ]
+        for words in testCases:
+            print('words: %s' % (words))
+            result = self.palindromePairs(words)
+            print('result: %s' % (result))
+            print('-='*20+'-')
+
+
+class Solution_ANOTHER(object):
+    # O (k^2 * N)
     def palindromePairs(self, words):
         if not words: return []
         hashmap = {}
@@ -45,6 +103,6 @@ class Solution(object):
             print('result: %s' % (result))
             print('-='*20+'-')
 
+
 if __name__ == '__main__':
     Solution().test()
-
