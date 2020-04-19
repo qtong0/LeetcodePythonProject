5cdf069eb87f1c8ce445d7f1d5dadcd7fc6460cd
Qiang Tong <tongqiang.atp@gmail.com>
2020-04-18 23:54:06 -0400

    add new solution

--- solutions/leetcode_1001_1050/LeetCode1008_ConstructBinarySearchTreeFromPreorderTraversal.py
+++ solutions/leetcode_1001_1050/LeetCode1008_ConstructBinarySearchTreeFromPreorderTraversal.py
@@ -10,16 +10,17 @@ class Solution:
     # O(N) solution
     def bstFromPreorder(self, preorder) -> TreeNode:
         idx = [0]
-        return self.helper(preorder, float('inf'), idx)
+        return self.helper(preorder, float('-inf'), float('inf'), idx)
 
-    def helper(self, preorder, upper, idx):
-        if idx[0] == len(preorder) or preorder[idx[0]] > upper:
+    def helper(self, preorder, lower, upper, idx):
+        if idx[0] < len(preorder) or preorder[idx[0]] < lower or preorder[idx[0]] > upper:
             return None
         root = TreeNode(preorder[idx[0]])
         idx[0] += 1
-        root.left = self.helper(preorder, root.val, idx)
-        root.right = self.helper(preorder, upper, idx)
+        root.left = self.helper(preorder, lower, root.val, idx)
+        root.right = self.helper(preorder, root.val, upper, idx)
         return root
+
 
     # My own O(N*Log(N)) solution, passing LC, should be fine too???
     def bstFromPreorder_own(self, preorder) -> TreeNode:
