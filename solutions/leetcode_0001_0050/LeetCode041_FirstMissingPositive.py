43be3fa3bfbd23447f65c365de0e75632bb11736
Qiang Tong <tongqiang.atp@gmail.com>
2020-04-19 19:14:12 -0400

    add new solution

--- solutions/leetcode_0001_0050/LeetCode041_FirstMissingPositive.py
+++ solutions/leetcode_0001_0050/LeetCode041_FirstMissingPositive.py
@@ -1,20 +1,35 @@
-'''
-Created on Jun 6, 2018
-
-@author: tongq
-'''
 class Solution(object):
-    def firstMissingPositive(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+    def firstMissingPositive(self, nums) -> int:
+        if 1 not in nums:
+            return 1
         n = len(nums)
-        dp = [0]*n
-        for num in nums:
-            if 0 <= num-1 < n:
-                dp[num-1] += 1
-        for i, val in enumerate(dp):
-            if val == 0:
-                return i+1
-        return len(nums)+1
+        for i in range(n):
+            if nums[i] <= 0 or nums[i] > n:
+                nums[i] = 1
+        for i in range(n):
+            num = abs(nums[i])
+            if num == n:
+                nums[0] = -abs(nums[0])
+            else:
+                nums[num] = -abs(nums[num])
+        for i in range(1, n):
+            if nums[i] > 0:
+                return i
+        if nums[0] > 0:
+            return n
+        return n+1
+
+    def test(self):
+        testCases = [
+            [1,2,0],
+            [3,4,-1,1],
+            [7,8,9,11,12],
+        ]
+        for nums in testCases:
+            res = self.firstMissingPositive(nums)
+            print('res: %s' % res)
+            print('-='*30+'-')
+
+
+if __name__ == '__main__':
+    Solution().test()
