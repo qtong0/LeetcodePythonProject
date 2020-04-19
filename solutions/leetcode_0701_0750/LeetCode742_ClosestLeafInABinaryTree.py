cc688b5ea3ef26e66814f08f37611278c9e52fa9
Qiang Tong <tongqiang.atp@gmail.com>
2020-04-18 20:49:58 -0400

    add new solution

--- solutions/leetcode_0701_0750/LeetCode742_ClosestLeafInABinaryTree.py
+++ solutions/leetcode_0701_0750/LeetCode742_ClosestLeafInABinaryTree.py
@@ -1,14 +1,10 @@
-'''
-Created on Mar 21, 2018
-
-@author: tongq
-'''
 # Definition for a binary tree node.
 class TreeNode(object):
     def __init__(self, x, left=None, right=None):
         self.val = x
         self.left = left
         self.right = right
+
 
 class Solution(object):
     def findClosestLeaf(self, root, k):
