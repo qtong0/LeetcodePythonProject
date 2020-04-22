b07de31a8f88913c3ed25ff9bdc327c7161c6fc8
Qiang Tong <tongqiang.atp@gmail.com>
2020-04-21 22:36:29 -0400

    add new solution

--- solutions/leetcode_1001_1050/LeetCode1033_MovingStonesUntilConsecutive.py
+++ solutions/leetcode_1001_1050/LeetCode1033_MovingStonesUntilConsecutive.py
@@ -4,7 +4,7 @@ class Solution:
         arr.sort()
         if arr[2] - arr[0] == 2:
             return [0, 0]
-        if min(arr[1]-arr[0], arr[2]-arr[1] <= 2):
+        if min(arr[1]-arr[0], arr[2]-arr[1]) <= 2:
             minMoves = 1
         else:
             minMoves = 2
