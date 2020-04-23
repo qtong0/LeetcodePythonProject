d4e4f770e2d6feea28ce02b4b30c51a9236c026d
Qiang Tong <tongqiang.atp@gmail.com>
2020-04-23 14:30:33 -0400

    add new solution

--- solutions/leetcode_0001_0050/LeetCode050_Pow.py
+++ solutions/leetcode_0001_0050/LeetCode050_Pow.py
@@ -1,16 +1,5 @@
-'''
-Created on Jan 21, 2017
-
-@author: MT
-'''
-
 class Solution(object):
-    def myPow(self, x, n):
-        """
-        :type x: float
-        :type n: int
-        :rtype: float
-        """
+    def myPow_rec(self, x, n):
         if n == 0:
             return 1
         if n < 0:
@@ -20,9 +9,24 @@ class Solution(object):
             return self.myPow(x*x, n/2)
         else:
             return x*self.myPow(x*x, n/2)
-    
-    def test(self):
-        pass
+
+
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        if n < 0:
+            n = -n
+            x = 1/x
+        res = 1
+        curProd = x
+        i = n
+        while i > 0:
+            if i % 2 == 1:
+                res = res * curProd
+            curProd = curProd * curProd
+            i //= 2
+        return res
+
 
 if __name__ == '__main__':
     Solution().test()
