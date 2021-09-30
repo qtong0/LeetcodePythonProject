eca4add677c70b2c343d2db33ae6f61f6fa1bf68
Qiang Tong <tongqiang.atp@gmail.com>
2021-09-30 00:11:49 -0400

    adding new solution(s)

--- solutions/leetcode_0551_0600/LeetCode552_StudentAttendanceRecordII.py
+++ solutions/leetcode_0551_0600/LeetCode552_StudentAttendanceRecordII.py
@@ -1,56 +1,34 @@
-'''
-Created on Aug 23, 2017
-
-@author: MT
-'''
-
 class Solution(object):
     def checkRecord(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        import numpy
-        matrix = numpy.matrix(
-            [
-                [0, 0, 1, 0, 0, 0],
-                [1, 0, 1, 0, 0, 0],
-                [0, 1, 1, 0, 0, 0],
-                [0, 0, 1, 0, 0, 1],
-                [0, 0, 1, 1, 0, 1],
-                [0, 0, 1, 0, 1, 1],
-            ]
-        )
-        power = matrix
-        mod = 10**9+7
-        while n:
-            if n & 1:
-                power = (power*matrix)%mod
-            matrix = matrix**2 % mod
-            n /= 2
-        return int(power[5, 2])
-    
-    def checkRecord_slow(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        mod = 10**9+7
-        dp = [[[0]*3 for _ in range(2)] for _ in range(n+1)]
-        for i in range(2):
-            for j in range(3):
-                dp[0][i][j] = 1
-        for i in range(1, n+1):
-            for j in range(2):
-                for k in range(3):
-                    val = dp[i-1][j][2]
-                    if j > 0:
-                        val = (val+dp[i-1][j-1][2]) % mod # A
-                    if k > 0:
-                        val = (val+dp[i-1][j][k-1]) % mod # L
-                    dp[i][j][k] = val
-        return dp[-1][-1][-1]
-    
+        mod = 10 ** 9 + 7
+        a = [0] * n
+        p = [0] * n
+        l = [0] * n
+
+        if n == 1:
+            return 3
+        if n == 2:
+            return 8
+
+        a[0] = 1
+        a[1] = 2
+        a[2] = 4
+        p[0] = 1
+        l[0] = 1
+        l[1] = 3
+
+        for i in range(1, n):
+            p[i] = (a[i - 1] + p[i - 1] + l[i - 1]) % mod
+            if i > 1:
+                l[i] = (a[i - 1] + p[i - 1] + a[i - 2] + p[i - 2]) % mod
+            if i > 2:
+                # A(n) = noAP(n - 1) + noAL(n - 1), n ≥ 2.
+                # noAP(n) = noAP(n - 1) + noAL(n - 1), n ≥ 2.
+                # noAL(n) = noAP(n - 1) + noAP(n - 2), n ≥ 3.
+                a[i] = (a[i - 1] + a[i - 2] + a[i - 3]) % mod
+
+        return (a[-1] + p[-1] + l[-1]) % mod
+
     def test(self):
         testCases = [
             2,
@@ -61,7 +39,8 @@ class Solution(object):
             print('n: %s' % n)
             result = self.checkRecord(n)
             print('result: %s' % result)
-            print('-='*30+'-')
+            print('-=' * 30 + '-')
+
 
 if __name__ == '__main__':
     Solution().test()
