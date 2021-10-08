33c11cfa5269b1e55f53500f38a137c9d71f62dc
Qiang Tong <tongqiang.atp@gmail.com>
2021-10-08 00:45:46 -0400

    adding new solution(s)

--- solutions/leetcode_1001_1050/LeetCode1049_LastStoneWeightII.py
+++ solutions/leetcode_1001_1050/LeetCode1049_LastStoneWeightII.py
@@ -2,8 +2,19 @@ from typing import List
 
 
 class Solution:
-    # Knapsacks Problem !!!
-    # DP
+    # !!! Knapsacks Problem !!!
+    #
+    # Why it's Knapsacks problem?
+    # One way to understand the fact that "The minimum result of cancellation = the minimum difference between the sum of two groups".
+    # Say you've already found two groups with smallest difference.
+    # Group A = [A1, A2, ..., An]
+    # Group B = [B1, B2, ..., Bm]
+    # The process we cancel two stones is to arbitrarily pick one from group A and one from Group B.
+    # If Ai > Bj, then put Ai-Bj into group A.
+    # If Ai < Bj, then put Ai-Bj into group B.
+    # If Ai = Bj, then nothing will be put into group A and B.
+    # We repeat the process until there is only one stone left. You will find the remaining stone is |sum(Group A) - sum(Group B)|.
+    #
     def lastStoneWeightII(self, stones: List[int]) -> int:
         sum_all = sum(stones)
         dp = [False]*(1 + sum_all//2)
@@ -12,7 +23,7 @@ class Solution:
             for i in range(sum_all//2, num-1, -1):
                 if dp[i-num]:
                     dp[i] = True
-        for i in range(sum_all // 2, -1, -1):
+        for i in range(sum_all//2, -1, -1):
             if dp[i]:
                 return sum_all - i - i
         return 0
