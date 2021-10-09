49e3a7733d21ded0bbcab427c084c6f96f730a33
Qiang Tong <tongqiang.atp@gmail.com>
2021-10-09 01:14:54 -0400

    adding new solution(s)

--- solutions/leetcode_0801_0850/LeetCode801_MinimumSwapsToMakeSequences.py
+++ solutions/leetcode_0801_0850/LeetCode801_MinimumSwapsToMakeSequences.py
@@ -1,6 +1,17 @@
 from typing import List
 
 
+# A[i - 1] < A[i] && B[i - 1] < B[i].
+# In this case, if we want to keep A and B increasing before the index i, can only have two choices.
+# -> 1.1 don't swap at (i-1) and don't swap at i, we can get not_swap[i] = not_swap[i-1]
+# -> 1.2 swap at (i-1) and swap at i, we can get swap[i] = swap[i-1]+1
+# if swap at (i-1) and do not swap at i, we can not guarantee A and B increasing.
+#
+# A[i-1] < B[i] && B[i-1] < A[i]
+# In this case, if we want to keep A and B increasing before the index i, can only have two choices.
+# -> 2.1 swap at (i-1) and do not swap at i, we can get notswap[i] = Math.min(swap[i-1], notswap[i] )
+# -> 2.2 do not swap at (i-1) and swap at i, we can get swap[i]=Math.min(notswap[i-1]+1, swap[i])
+#
 class Solution:
     # Time O(N)
     # Space O(N)
