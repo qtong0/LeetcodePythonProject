20589614b18f493b8be78f15e784617942290831
Qiang Tong <tongqiang.atp@gmail.com>
2021-10-09 01:55:16 -0400

    adding new solution(s)

--- solutions/leetcode_0201_0250/LeetCode233_NumberOfDigitOne.py
+++ solutions/leetcode_0201_0250/LeetCode233_NumberOfDigitOne.py
@@ -1,23 +1,39 @@
-'''
-Created on Feb 23, 2017
-
-@author: MT
-'''
 class Solution(object):
-    def countDigitOne(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        if n <= 0: return 0
-        q, x, res = n, 1, 0
-        while q > 0:
-            digit = q%10
-            q //= 10
-            res += q*x
-            if digit == 1:
-                res += n%x+1
-            elif digit > 1:
-                res += x
-            x *= 10
-        return res
+    # traverse each digit of n, if n=3401512
+    #  for m=100, split n into a=34015 and b=12
+    #  if a%10==1, #1=a/10*m+(b+1);
+    #  if a%10==0, #2=a/10*m;
+    #  if a%10>1; #3=a/10*m+m;
+    #  In general, #4=(a+8)/10*m+(a % 10 == 1)*(b + 1).
+    #
+    # For general expression above:
+    #  if a%10>1, then (a+8)/10=a/10+1, #4=#3
+    #  if a%10==0, (a+8)/10=a/10, (a % 10 == 1)=false, #4=#2
+    #  if a%10==1, (a+8)/10=a/10, (a % 10 == 1)=true, #4=#1
+    #
+    def countDigitOne(self, n: int) -> int:
+        ones = 0
+        m = 1
+        while m <= n:
+            a = n // m
+            b = n % m
+
+            ones += (a + 8) // 10 * m
+            if a % 10 == 1:
+                ones += b+1
+            m *= 10
+        return ones
+
+    def test(self):
+        test_cases = [
+            13,
+            0,
+        ]
+        for n in test_cases:
+            res = self.countDigitOne(n)
+            print('res: %s' % res)
+            print('-='*30 + '-')
+
+
+if __name__ == '__main__':
+    Solution().test()
