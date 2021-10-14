2cb6ac17fa0d53e52230b7ee52769dfa70d9e097
Qiang Tong <tongqiang.atp@gmail.com>
2021-10-14 02:00:38 -0400

    adding new solution(s)

--- solutions/leetcode_0851_0900/LeetCode0857_MinimumCostToHireKWorkers.py
+++ solutions/leetcode_0851_0900/LeetCode0857_MinimumCostToHireKWorkers.py
@@ -1,16 +1,8 @@
-'''
-Created on Sep 16, 2019
+from typing import List
 
-@author: tongq
-'''
+
 class Solution(object):
-    def mincostToHireWorkers(self, quality, wage, K):
-        """
-        :type quality: List[int]
-        :type wage: List[int]
-        :type K: int
-        :rtype: float
-        """
+    def mincostToHireWorkers(self, quality: List[int], wage: List[int], K: int) -> float:
         import heapq
         k = K
         workers = sorted([float(w)/q, q] for w, q in zip(wage, quality))
@@ -28,12 +20,14 @@ class Solution(object):
     
     def test(self):
         testCases = [
-            
+            [[10,20,5], [70,50,30], 2],
+            [[3,1,10,10,1], [4,8,2,2,7], 3],
         ]
         for quality, wage, k in testCases:
             res = self.mincostToHireWorkers(quality, wage, k)
             print('res: %s' % res)
             print('-='*30 + '-')
+
 
 if __name__ == '__main__':
     Solution().test()
