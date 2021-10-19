14ecd652da16d323b88541280d261d453e37a3cf
Qiang Tong <tongqiang.atp@gmail.com>
2021-10-19 02:29:17 -0400

    adding new solution(s)

--- solutions/leetcode_0951_1000/LeetCode0975_OddEvenJump.py
+++ solutions/leetcode_0951_1000/LeetCode0975_OddEvenJump.py
@@ -1,9 +1,45 @@
+from typing import List
+
+
 class Solution:
-    def oddEvenJumps(self, A) -> int:
-        pass
+    def oddEvenJumps(self, arr: List[int]) -> int:
+        n = len(arr)
+        next_higher, next_lower = [0]*n, [0]*n
 
-# Checkout Java TreeMap solution:
+        stack = []
+        for a, i in sorted([a, i] for i, a in enumerate(arr)):
+            while stack and stack[-1] < i:
+                next_higher[stack.pop()] = i
+            stack.append(i)
 
+        stack = []
+        for a, i in sorted([-a, i] for i, a in enumerate(arr)):
+            while stack and stack[-1] < i:
+                next_lower[stack.pop()] = i
+            stack.append(i)
+
+        higher, lower = [0]*n, [0]*n
+        higher[-1] = lower[-1] = 1
+        for i in range(n-2, -1, -1):
+            higher[i] = lower[next_higher[i]]
+            lower[i] = higher[next_lower[i]]
+        return sum(higher)
+
+
+
+    def test(self):
+        test_cases = [
+            [10,13,12,14,15],
+            [2,3,1,1,4],
+        ]
+        for arr in test_cases:
+            res = self.oddEvenJumps(arr)
+            print('res: %s' % res)
+            print('-='*30 + '-')
+
+
+
+# Another solution is using TreeMap:
 
 """
 public class LeetCode0975_OddEvenJump {
@@ -42,3 +78,8 @@ public class LeetCode0975_OddEvenJump {
 }
 
 """
+
+
+
+if __name__ == '__main__':
+    Solution().test()
