34ef1135ef8800797f3a464824d3b34df66a48ec
Qiang Tong <tongqiang.atp@gmail.com>
2021-11-08 00:28:29 -0500

    adding new solution(s)

--- solutions/leetcode_0751_0800/LeetCode777_SwapAdjacentInLRString.py
+++ solutions/leetcode_0751_0800/LeetCode777_SwapAdjacentInLRString.py
@@ -1,36 +1,26 @@
-'''
-Created on Apr 8, 2018
-
-@author: tongq
-'''
 class Solution(object):
-    def canTransform(self, start, end):
-        """
-        :type start: str
-        :type end: str
-        :rtype: bool
-        """
-        l, r = 0, 0
-        for i in range(len(start)):
-            if start[i] == 'R': r += 1
-            if end[i] == 'L': l += 1
-            if start[i] == 'L': l -= 1
-            if end[i] == 'R': r -= 1
-            if (l < 0 or r != 0) and (l != 0 or r < 0):
+    def canTransform(self, start: str, end: str) -> bool:
+        if len(start) != len(end):
+            return False
+        arr_start = [(idx, c) for idx, c in enumerate(start) if c in 'LR']
+        arr_end = [(idx, c) for idx, c in enumerate(end) if c in 'LR']
+        if len(arr_start) != len(arr_end):
+            return False
+
+        for (i, cs), (j, ce) in zip(arr_start, arr_end):
+            if cs != ce:
                 return False
-        if l == 0 and r == 0:
-            return True
-        return False
+            if cs == 'L':
+                if i < j:
+                    return False
+            if ce == 'R':
+                if i > j:
+                    return False
+        return True
 
 
     # The following BFS solution is TLE
-    
-    def canTransform_bfs_TLE(self, start, end):
-        """
-        :type start: str
-        :type end: str
-        :rtype: bool
-        """
+    def canTransform_bfs_TLE(self, start: str, end: str) -> bool:
         visited = set([start])
         queue = [start]
         while queue:
@@ -44,9 +34,14 @@ class Solution(object):
                         visited.add(newS)
                         queue.append(newS)
         return False
-    
+
+
     def test(self):
         testCases = [
+            [
+                "RLX",
+                "XLR",
+            ],
             [
                 "RXXLRXRXL",
                 "XRLXXRRLX",
@@ -62,6 +57,7 @@ class Solution(object):
             result = self.canTransform(start, end)
             print('result: %s' % result)
             print('-='*30+'-')
+
 
 if __name__ == '__main__':
     Solution().test()
