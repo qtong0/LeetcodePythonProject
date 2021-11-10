e6d38de63cd0e3a692079554a3c0ba426ca4820d
Qiang Tong <tongqiang.atp@gmail.com>
2021-11-10 01:03:59 -0500

    adding new solution(s)

--- solutions/leetcode_0901_0950/LeetCode0947_MostStonesRemovedWithSameRowOrColumn.py
+++ solutions/leetcode_0901_0950/LeetCode0947_MostStonesRemovedWithSameRowOrColumn.py
@@ -1,26 +1,32 @@
-class DSU:
-    def __init__(self, n):
-        self.roots = range(n)
-
-    def find(self, x):
-        while x != self.roots[x]:
-            x = self.roots[x]
-        return self.roots[x]
-
-    def union(self, x, y):
-        xr = self.find(x)
-        yr = self.find(y)
-        self.roots[xr] = yr
+from typing import List
 
 
 class Solution(object):
-    def removeStones(self, stones):
-        """
-        :type stones: List[List[int]]
-        :rtype: int
-        """
-        n = len(stones)
-        dsu = DSU(20000)
-        for x, y in stones:
-            dsu.union(x, y+10000)
-        return n - len({dsu.find(x) for x, y in stones})
+    def removeStones(self, stones: List[List[int]]) -> int:
+        union_find = {}
+        for i, j in stones:
+            root1 = self.find(union_find, i)
+            root2 = self.find(union_find, ~j)
+            union_find[root2] = root1
+        return len(stones) - len({self.find(union_find, x) for x in union_find})
+
+    def find(self, union_find, x):
+        while x != union_find.setdefault(x, x):
+            x = union_find[x]
+        return x
+
+
+    def test(self):
+        test_cases = [
+            [[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]],
+            [[0,0],[0,2],[1,1],[2,0],[2,2]],
+            [[0,0]],
+        ]
+        for stones in test_cases:
+            res = self.removeStones(stones)
+            print('res: %s' % res)
+            print('-='*30 + '-')
+
+
+if __name__ == '__main__':
+    Solution().test()
