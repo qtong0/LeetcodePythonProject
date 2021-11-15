246cfef90bf9f5c85a60a21cc19849e2e9ca30e1
Qiang Tong <tongqiang.atp@gmail.com>
2021-11-15 01:46:10 -0500

    adding new solution(s)

---
+++ solutions/leetcode_1001_1050/LeetCode1044_LongestDuplicateSubstring.py
@@ -0,0 +1,42 @@
+from functools import reduce
+
+
+class Solution:
+    def longestDupSubstring(self, s: str) -> str:
+        A = [ord(c) - ord('a') for c in s]
+        mod = 2**63 - 1
+
+        def test(L):
+            p = pow(26, L, mod)
+            cur = reduce(lambda x, y: (x * 26 + y) % mod, A[:L], 0)
+            seen = {cur}
+            for i in range(L, len(s)):
+                cur = (cur * 26 + A[i] - A[i - L] * p) % mod
+                if cur in seen: return i - L + 1
+                seen.add(cur)
+        res, lo, hi = 0, 0, len(s)
+        while lo < hi:
+            mi = (lo + hi + 1) // 2
+            pos = test(mi)
+            if pos:
+                lo = mi
+                res = pos
+            else:
+                hi = mi - 1
+        return s[res:res + lo]
+
+
+    def test(self):
+        test_caes = [
+            'aa',
+            'banana',
+            'abcd',
+        ]
+        for s in test_caes:
+            res = self.longestDupSubstring(s)
+            print('res: %s' % res)
+            print('-='*30 + '-')
+
+
+if __name__ == '__main__':
+    Solution().test()
