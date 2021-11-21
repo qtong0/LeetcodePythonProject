742f1cf614a078ca46d1c46510421bf7e0047757
Qiang Tong <tongqiang.atp@gmail.com>
2021-11-21 01:46:56 -0500

    adding new solution(s)

--- solutions/leetcode_0801_0850/LeetCode818_RaceCar.py
+++ solutions/leetcode_0801_0850/LeetCode818_RaceCar.py
@@ -12,20 +12,22 @@ class Solution(object):
         else:
             self.dp[target] = self.racecar(2**n-1-target)+n+1
             for m in range(n-1):
-                self.dp[target] = min(self.dp[target],\
-                            self.racecar(target-2**(n-1)+2**m)+n+m+1)
+                self.dp[target] = min(self.dp[target],
+                                      self.racecar(target-2**(n-1)+2**m)+n+m+1)
         return self.dp[target]
-    
+
+
     def test(self):
-        testCases = [
+        test_cases = [
+            1,
             3,
             6,
         ]
-        for target in testCases:
-            print('target: %s' % target)
-            result = self.racecar(target)
-            print('result: %s' % result)
+        for target in test_cases:
+            res1 = self.racecar(target)
+            print('res1: %s' % res1)
             print('-='*30+'-')
+
 
 if __name__ == '__main__':
     Solution().test()
