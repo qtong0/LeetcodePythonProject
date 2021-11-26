25f21beb5822b7cae980b0b20688010d21c08d4e
Qiang Tong <tongqiang.atp@gmail.com>
2021-11-26 15:51:13 -0500

    adding new solution(s)

--- solutions/leetcode_1601_1650/LeetCode1632_RankTransformOfAMatrix.py
+++ solutions/leetcode_1601_1650/LeetCode1632_RankTransformOfAMatrix.py
@@ -2,7 +2,57 @@ import collections
 from typing import List
 
 
+class UnionFind:
+    def __init__(self):
+        self.parent = {}
+
+    def find(self, u):
+        if u != self.parent[u]:
+            self.parent[u] = self.find(self.parent[u])
+        return self.parent[u]
+
+    def union(self, u, v):
+        self.parent.setdefault(u, u)
+        self.parent.setdefault(v, v)
+        pu, pv = self.find(u), self.find(v)
+        if pu != pv: self.parent[pu] = pv
+
+    def getGroups(self):
+        groups = collections.defaultdict(list)
+        for i in self.parent.keys():
+            groups[self.find(i)].append(i)
+        return groups
+
+
 class Solution:
+    def matrixRankTransform(self, matrix: List[List[int]]) -> List[List[int]]:
+        m, n = len(matrix), len(matrix[0])
+        d = collections.defaultdict(list)
+
+        for r in range(m):
+            for c in range(n):
+                d[matrix[r][c]].append([r, c])
+
+        rank = [0] * (m + n)  # rank[i] is the largest rank of the row or column so far.
+        for a in sorted(d):
+            uf = UnionFind()
+
+            for r, c in d[a]:
+                uf.union(r, c + m)  # Union row `r` with column `c` (column +m to separate with r)
+
+            for group in uf.getGroups().values():
+                maxRank = max(rank[i] for i in group)  # Get max rank of all included rows and columns
+                for i in group: rank[i] = maxRank + 1  # Update all rows or columns in the same groups to new rank
+
+            for r, c in d[a]:
+                matrix[r][c] = rank[r]  # or matrix[r][c] = rank[c+m], both are correct!
+
+        return matrix
+
+
+
+
+class Solution_another:
     # TC: O(NNLog(MN))
     # SC: O(MN)
     #
@@ -46,4 +96,4 @@ class Solution:
 
 
 if __name__ == '__main__':
-    Solution().test()
+    Solution_another().test()
