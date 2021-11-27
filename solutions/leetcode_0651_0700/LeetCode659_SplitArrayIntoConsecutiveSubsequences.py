231614fc57a8c535a929461c4448e059a866cf35
Qiang Tong <tongqiang.atp@gmail.com>
2021-11-27 16:14:54 -0500

    adding new solution(s)

--- solutions/leetcode_0651_0700/LeetCode659_SplitArrayIntoConsecutiveSubsequences.py
+++ solutions/leetcode_0651_0700/LeetCode659_SplitArrayIntoConsecutiveSubsequences.py
@@ -7,20 +7,25 @@ class Solution(object):
         for num in nums:
             freqMap[num] = freqMap.get(num, 0)+1
         for num in nums:
+            # if num is already used some sequence
             if freqMap[num] == 0:
                 continue
+            # if num can be added in the last consecutive sequence
             elif appendFreqMap.get(num, 0) > 0:
                 appendFreqMap[num] -= 1
-                appendFreqMap[num+1] = appendFreqMap.get(num+1, 0)+1
-            elif freqMap.get(num+1, 0)>0 and freqMap.get(num+2, 0)>0:
+                appendFreqMap[num+1] = appendFreqMap.get(num+1, 0) + 1
+            # this num should form another consecutive sequence by itself
+            # because it cannot be appended to a previous sequence
+            elif freqMap.get(num+1, 0) > 0 and freqMap.get(num+2, 0) > 0:
                 freqMap[num+1] -= 1
                 freqMap[num+2] -= 1
-                appendFreqMap[num+3] = appendFreqMap.get(num+3, 0)+1
+                appendFreqMap[num+3] = appendFreqMap.get(num+3, 0) + 1
             else:
                 return False
             freqMap[num] -= 1
         return True
-    
+
+
     def test(self):
         testCases = [
             [1, 2, 3, 3, 4, 5],
