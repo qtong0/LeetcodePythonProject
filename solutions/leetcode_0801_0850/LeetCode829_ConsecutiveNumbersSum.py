83ee848a12ae0d25868d9c311b5bd2049a9e4752
Qiang Tong <tongqiang.atp@gmail.com>
2021-11-26 23:32:05 -0500

    adding new solution(s)

--- solutions/leetcode_0801_0850/LeetCode829_ConsecutiveNumbersSum.py
+++ solutions/leetcode_0801_0850/LeetCode829_ConsecutiveNumbersSum.py
@@ -1,22 +1,12 @@
-'''
-Created on May 6, 2018
-
-@author: tongq
-'''
-class Solution(object):
-    def consecutiveNumbersSum(self, N):
-        """
-        :type N: int
-        :rtype: int
-        """
-        n = N
+class Solution:
+    def consecutiveNumbersSum(self, n: int) -> int:
         res = 1
         i = 3
-        while n%2 == 0:
+        while n % 2 == 0:
             n /= 2
         while i*i <= n:
             count = 0
-            while n%i == 0:
+            while n % i == 0:
                 n //= i
                 count += 1
             res *= count+1
