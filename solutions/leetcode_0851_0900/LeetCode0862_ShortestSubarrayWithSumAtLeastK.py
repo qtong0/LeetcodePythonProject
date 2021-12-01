505066466498946172d6f528cfcf4a8da9d4eb95
Qiang Tong <tongqiang.atp@gmail.com>
2021-12-01 01:09:00 -0500

    adding new solution(s)

--- solutions/leetcode_0851_0900/LeetCode0862_ShortestSubarrayWithSumAtLeastK.py
+++ solutions/leetcode_0851_0900/LeetCode0862_ShortestSubarrayWithSumAtLeastK.py
@@ -1,28 +1,20 @@
-'''
-Created on Sep 17, 2019
+from typing import List
 
-@author: tongq
-'''
+
 class Solution(object):
-    def shortestSubarray(self, A, K):
-        """
-        :type A: List[int]
-        :type K: int
-        :rtype: int
-        """
-        arr, k = A, K
-        n = len(arr)
-        arr2 = [0]*(n+1)
+    def shortestSubarray(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        preSums = [0]*(n+1)
         for i in range(n):
-            arr2[i+1] = arr2[i] + arr[i]
-        d = []
+            preSums[i+1] = preSums[i] + nums[i]
+        deque = []
         res = n+1
         for i in range(n+1):
-            while d and arr2[i] - arr2[d[0]] >= k:
-                res = min(res, i-d.pop(0))
-            while d and arr2[i] <= arr2[d[-1]]:
-                d.pop()
-            d.append(i)
+            while deque and preSums[i] - preSums[deque[0]] >= k:
+                res = min(res, i-deque.pop(0))
+            while deque and preSums[i] <= preSums[deque[-1]]:
+                deque.pop()
+            deque.append(i)
         return res if res <= n else -1
     
     def test(self):
@@ -31,35 +23,36 @@ class Solution(object):
                 [56,-21,56,35,-9],
                 61,
             ],
-#             [
-#                 [84,-37,32,40,95],
-#                 167,
-#             ],
-#             [
-#                 [1],
-#                 1,
-#             ],
-#             [
-#                 [1,2],
-#                 4
-#             ],
-#             [
-#                 [2,-1,2],
-#                 3
-#             ],
-#             [
-#                 [77,19,35,10,-14],
-#                 19
-#             ],
-#             [
-#                 [-3, 4, 1],
-#                 5,
-#             ],
+            [
+                [84,-37,32,40,95],
+                167,
+            ],
+            [
+                [1],
+                1,
+            ],
+            [
+                [1,2],
+                4
+            ],
+            [
+                [2,-1,2],
+                3
+            ],
+            [
+                [77,19,35,10,-14],
+                19
+            ],
+            [
+                [-3, 4, 1],
+                5,
+            ],
         ]
-        for a, k in testCase:
-            res = self.shortestSubarray(a, k)
+        for nums, k in testCase:
+            res = self.shortestSubarray(nums, k)
             print('res: %s' % res)
             print('-='*30+'-')
+
 
 if __name__ == '__main__':
     Solution().test()
