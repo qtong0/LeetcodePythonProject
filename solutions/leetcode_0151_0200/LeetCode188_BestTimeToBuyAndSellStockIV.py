d0bf8f91e4c7c8a4cd7400e65b055e54c1a177d6
Qiang Tong <tongqiang.atp@gmail.com>
2021-12-04 00:03:53 -0500

    adding new solution(s)

--- solutions/leetcode_0151_0200/LeetCode188_BestTimeToBuyAndSellStockIV.py
+++ solutions/leetcode_0151_0200/LeetCode188_BestTimeToBuyAndSellStockIV.py
@@ -1,16 +1,8 @@
-'''
-Created on Feb 16, 2017
+from typing import List
 
-@author: MT
-'''
 
 class Solution(object):
-    def maxProfit(self, k, prices):
-        """
-        :type k: int
-        :type prices: List[int]
-        :rtype: int
-        """
+    def maxProfit(self, k: int, prices: List[int]) -> int:
         n = len(prices)
         if k*2 >= n:
             return self.quickSolve(prices)
@@ -21,13 +13,14 @@ class Solution(object):
                 dp[i][j] = max(dp[i][j-1], tmpMax+prices[j])
                 tmpMax = max(tmpMax, dp[i-1][j-1]-prices[j])
         return dp[-1][-1]
-    
+
     def quickSolve(self, prices):
         res = 0
         for i in range(1, len(prices)):
             res += max(0, prices[i]-prices[i-1])
         return res
-    
+
+
     def test(self):
         testCases = [
             ([2, 1], 1),
@@ -39,6 +32,7 @@ class Solution(object):
             result = self.maxProfit(k, prices)
             print('result: %s' % (result))
             print('-='*20+'-')
+
 
 if __name__ == '__main__':
     Solution().test()
