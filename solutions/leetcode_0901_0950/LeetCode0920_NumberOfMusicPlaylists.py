a75787b980b9ffaf67d5bc7ecadbe1d7086f4945
Qiang Tong <tongqiang.atp@gmail.com>
2021-12-03 23:50:52 -0500

    adding new solution(s)

--- solutions/leetcode_0901_0950/LeetCode0920_NumberOfMusicPlaylists.py
+++ solutions/leetcode_0901_0950/LeetCode0920_NumberOfMusicPlaylists.py
@@ -5,20 +5,27 @@ class Solution(object):
         dp[0][0] = 1
         for i in range(1, goal+1):
             for j in range(1, n+1):
-                dp[i][j] += dp[i-1][j-1]*(n-j+1)
-                dp[i][j] += dp[i-1][j]*max(j-k, 0)
+                # (the last added one is new song):
+                # listen i - 1 songs with j - 1 different songs
+                # then the last one is definitely new song with the choices of N - (j - 1).
+                dp[i][j] += dp[i-1][j-1]*(n - (j - 1))
+                # (the last added one is old song):
+                # listen i - 1 songs with j different songs
+                # then the last one is definitely old song with the choices of j
+                if j > k:
+                    dp[i][j] += dp[i-1][j] * (j - k)
                 dp[i][j] %= MOD
         return dp[goal][n]
 
 
     def test(self):
-        testCases = [
+        test_cases = [
             [1, 3, 0],
             [3, 3, 1],
             [2, 3, 0],
             [2, 3, 1],
         ]
-        for n, l, k in testCases:
+        for n, l, k in test_cases:
             res = self.numMusicPlaylists(n, l, k)
             print('res: %s' % res)
             print('-='*30+'-')
