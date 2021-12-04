f4aa7f6b78fe7f3147a937fda2700568bef71b53
Qiang Tong <tongqiang.atp@gmail.com>
2021-12-03 23:27:06 -0500

    adding new solution(s)

--- solutions/leetcode_0901_0950/LeetCode0920_NumberOfMusicPlaylists.py
+++ solutions/leetcode_0901_0950/LeetCode0920_NumberOfMusicPlaylists.py
@@ -1,20 +1,15 @@
 class Solution(object):
-    def numMusicPlaylists(self, N, L, K):
-        """
-        :type N: int
-        :type L: int
-        :type K: int
-        :rtype: int
-        """
+    def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
         MOD = 10**9+7
-        dp = [[0]*(N+1) for _ in range(L+1)]
+        dp = [[0]*(n+1) for _ in range(goal+1)]
         dp[0][0] = 1
-        for i in range(1, L+1):
-            for j in range(1, N+1):
-                dp[i][j] += dp[i-1][j-1]*(N-j+1)
-                dp[i][j] += dp[i-1][j]*max(j-K, 0)
+        for i in range(1, goal+1):
+            for j in range(1, n+1):
+                dp[i][j] += dp[i-1][j-1]*(n-j+1)
+                dp[i][j] += dp[i-1][j]*max(j-k, 0)
                 dp[i][j] %= MOD
-        return dp[L][N]
+        return dp[goal][n]
+
 
     def test(self):
         testCases = [
