5ba986145972b1552df14c40c2b82fe4216413b0
Qiang Tong <tongqiang.atp@gmail.com>
2021-12-04 15:31:29 -0500

    adding new solution(s)

--- solutions/leetcode_0351_0400/LeetCode358_RearrangeStringKDistanceApart.py
+++ solutions/leetcode_0351_0400/LeetCode358_RearrangeStringKDistanceApart.py
@@ -1,13 +1,8 @@
-'''
-Created on Mar 23, 2017
-
-@author: MT
-'''
-
 import heapq
 
+
 class Solution(object):
-    def rearrangeString(self, s, k):
+    def rearrangeString(self, s: str, k: int) -> str:
         hashmap = {}
         for c in s:
             hashmap[c] = hashmap.get(c, 0)+1
@@ -27,7 +22,8 @@ class Solution(object):
             if freq > 0:
                 heapq.heappush(heap, [-freq, c])
         return ''.join(res) if len(res) == len(s) else ''
-    
+
+
     def test(self):
         testCases = [
             ('aabbcc', 3),
@@ -40,6 +36,7 @@ class Solution(object):
             result = self.rearrangeString(s, k)
             print('result: %s' % (result))
             print('-='*20+'-')
+
 
 if __name__ == '__main__':
     Solution().test()
