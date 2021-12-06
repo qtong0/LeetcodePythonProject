69341a25cebb105f2f0c0fc01791ba42ce75f679
Qiang Tong <tongqiang.atp@gmail.com>
2021-12-05 21:01:17 -0500

    adding new solution(s)

--- solutions/leetcode_0651_0700/LeetCode686_RepeatedStringMatch.py
+++ solutions/leetcode_0651_0700/LeetCode686_RepeatedStringMatch.py
@@ -1,24 +1,14 @@
-'''
-Created on Oct 22, 2017
-
-@author: MT
-'''
 class Solution(object):
-    def repeatedStringMatch(self, A, B):
-        """
-        :type A: str
-        :type B: str
-        :rtype: int
-        """
-        s1, s2 = A, B
-        n1, n2 = len(s1), len(s2)
+    def repeatedStringMatch(self, a: str, b: str) -> int:
+        n1, n2 = len(a), len(b)
         k = n2//n1+2
-        if str(s1*k).count(s2)==0:
+        if str(a*k).count(b) == 0:
             return -1
-        while k >= 1 and str(s1*k).count(s2)!=0:
+        while k >= 1 and str(a*k).count(b) != 0:
             k -= 1
         return k+1
-    
+
+
     def test(self):
         testCases = [
             [
@@ -32,6 +22,7 @@ class Solution(object):
             result = self.repeatedStringMatch(A, B)
             print('result: %s' % result)
             print('-='*30+'-')
+
 
 if __name__ == '__main__':
     Solution().test()
