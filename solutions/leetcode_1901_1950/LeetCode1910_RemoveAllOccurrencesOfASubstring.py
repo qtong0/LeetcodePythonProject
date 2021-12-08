f449a9c4ae90371cb1e94bb198cfe6d42edd692b
Qiang Tong <tongqiang.atp@gmail.com>
2021-12-08 15:44:36 -0500

    adding new solution(s)

--- solutions/leetcode_1901_1950/LeetCode1910_RemoveAllOccurrencesOfASubstring.py
+++ solutions/leetcode_1901_1950/LeetCode1910_RemoveAllOccurrencesOfASubstring.py
@@ -1,4 +1,37 @@
 class Solution:
+
+    # KMP Solution
+    def removeOccurrences_kmp(self, s: str, part: str) -> str:
+        kmp = [0]*(len(part) + 1)
+        idx = [0]*len(s)
+        i, j = 1, 0
+        while i < len(part):
+            if part[i] == part[j]:
+                kmp[i] = j
+                i += 1
+                j += 1
+            else:
+                if j == 0:
+                    i += 1
+                j = kmp[j]
+        d = 0
+        i, j = 0, 0
+        arr = list(s)
+        while i < len(arr):
+            arr[i-d] = arr[i]
+            if arr[i-d] == part[j]:
+                idx[i-d] = j+1
+                j += 1
+                if j == len(part):
+                    d += len(part)
+                    j = idx[i-d] if i >= d else 0
+            else:
+                if j != 0:
+                    j = kmp[j]
+                    i -= 1
+            i += 1
+        return ''.join(arr[:len(arr) - d])
+
     # Stack solution
     # The best solution is KMP - but I don't think this might be good enough
     def removeOccurrences(self, s: str, part: str) -> str:
@@ -30,7 +63,9 @@ class Solution:
         ]
         for s, part in test_cases:
             res = self.removeOccurrences(s, part)
+            res2 = self.removeOccurrences_kmp(s, part)
             print('res: %s' % res)
+            print('res2: %s' % res2)
             print('-='*30 + '-')
 
 
